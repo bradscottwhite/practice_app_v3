572f0a3dff0429b9432f60bae2395dea1431d1c4
Brad White <bradscottwhite@gmail.com>
2022-07-11 21:55:05 -0400

    Initial commit

---
+++ src/main/java/com/practice_app_v3/practice_app_v3/PracticeAppV3Application.java
@@ -0,0 +1,13 @@
+package com.practice_app_v3.practice_app_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeAppV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeAppV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice_app_v3/practice_app_v3/PracticeAppV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice_app_v3.practice_app_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeAppV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
